d72d114912838f276994e7e9fa5d0a3864de41bf
unknown <jaiswalsanket244@gmail.com>
2024-07-15 13:44:22 +0530

    Updated Cors

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const { connectDB } = require('./db');
+const cors = require('cors'); 
 const User_Router = require('./Routes/User_routes');
 const { Train_Route } = require('./Routes/Train_routes');
 require('dotenv').config();
 const cookieparser = require('cookie-parser')
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(cookieparser())
+
 // Use the User Router for the /user route
 app.use('/user', User_Router);
 // Use the Train TRouter for the /train route
